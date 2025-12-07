d74f94ffe5e747c72602337d4d3398fb48df6fd2
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-08 02:17:20 +0900

    design: 색상 정의 파일 업로드

---
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/ui/AppColors.kt
@@ -0,0 +1,23 @@
+package com.example.healthmeconverttocomposablecode.ui.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val mainColor = Color(0xff6A73FF)
+    val white = Color(0xffffffff)
+
+    val buttonColor = Color(0xffE4FF3E)
+    val buttonBorder = Color(0xff9BB400)
+
+    val textColor = Color(0xff409100)
+
+    val startColor = Color(0xFFF3FFFB) // F3FFFB
+    val endColor = Color(0xFFB8FFEA)   // B8FFEA
+
+    val firstScreenTextBrush = Brush.verticalGradient(
+        colors = listOf(startColor, endColor),
+        startY =0f,
+        endY =Float.POSITIVE_INFINITY
+    )
+}
