353b84e7d3b4b8bbfc8e779c01adaec5d5d6608b
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-15 02:49:18 +0900

    feat: 비밀번호 설정 화면 Root 구현

---
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/set_password/SetPasswordRoot.kt
@@ -0,0 +1,26 @@
+package com.example.healthmeconverttocomposablecode.presentation.auth.signup.set_password
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun SetPasswordRoot(
+    email: String,
+    viewModel: SetPasswordViewModel = viewModel(factory = SetPasswordViewModel.Factory)
+) {
+    val state by viewModel.state.collectAsState()
+    SetPasswordScreen(
+        state = state,
+        onPasswordChanged = {
+            viewModel.isSatisfyRule(it, isConfirmPassword = false)
+        },
+        onPasswordConfirmChanged = {
+            viewModel.isSatisfyRule(it, isConfirmPassword = true)
+        }
+
+    )
+
+
+}
